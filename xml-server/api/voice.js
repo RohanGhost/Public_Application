46f684c243c5aacb555128f3255a2135b1bc2340
Rohan Nagrut - Marico India Limited <rohan.nagrut@marico.com>
2026-01-19 16:13:45 +0530

    Clean minimal setup for Vercel serverless function

--- xml-server/api/voice.js
+++ xml-server/api/voice.js
@@ -1,10 +1,9 @@
+// api/voice.js
 export default function handler(req, res) {
   res.setHeader("Content-Type", "application/xml; charset=utf-8");
-
   const xml = `
 <Response>
   <Speak voice="WOMAN">Test OK</Speak>
 </Response>`.trim();
-
   res.status(200).send(xml);
 }
